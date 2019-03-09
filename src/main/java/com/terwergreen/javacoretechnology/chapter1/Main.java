6e28c358330cc8e74293d7d8cb4524db59835fb4
terwer <youweics@sina.com>
2019-03-09 18:53:58 +0800

    添加Java核心技术

---
+++ src/main/java/com/terwergreen/javacoretechnology/chapter1/Main.java
@@ -0,0 +1,14 @@
+package com.terwergreen.javacoretechnology.chapter1;
+
+/**
+ * Main
+ *
+ * @author Terwer
+ * @version 1.0
+ * 19-3-9 下午6:46
+ **/
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("args = " + args);
+    }
+}
