2576f614cc213b6be458bd110ddd93d565ec9979
Terwer <youweics@sina.com>
2019-04-02 10:49:35 +0800

    Add remote login

---
+++ src/main/java/com/terwergreen/masteroraclesql/remote.login.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+ssh oracle@39.104.66.135
+#123456
+sqlplus system/oracle@localhost:1521/xe
+SQL> conn system/oracle
---
+++ src/main/java/com/terwergreen/masteroraclesql/util.sql
@@ -0,0 +1,8 @@
+-- noinspection SqlResolveForFile
+-- samples
+-- https://github.com/oracle/db-sample-schemas
+-- 查看oracle版本
+select * from product_component_version;
+
+-- 删除用户以及下面的objects
+-- DROP USER hr CASCADE;
