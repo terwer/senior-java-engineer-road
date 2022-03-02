9e3bb4449b681a6d78886a8d7ce9ea1422f8d605
Terwer <youweics@163.com>
2022-03-02 20:16:05 +0800

    JVM学习大纲

---
+++ p7-skill/jvm/jvm-demo/src/main/java/com/terwergreen/App.java
@@ -0,0 +1,21 @@
+package com.terwergreen;
+
+import java.io.DataInput;
+import java.io.DataInputStream;
+import java.io.DataOutput;
+import java.io.DataOutputStream;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+
+        // DataInput;
+        // DataOutput
+        // DataInputStream
+        // DataOutputStream
+
+    }
+}
