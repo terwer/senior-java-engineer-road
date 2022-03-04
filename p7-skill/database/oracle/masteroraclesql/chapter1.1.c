03ba448a192aeb198134eebb72092931b17d4a82
Terwer <youweics@163.com>
2022-03-05 02:51:42 +0800

    修正语法错误

--- p7-skill/database/oracle/masteroraclesql/chapter1.1.c
+++ p7-skill/database/oracle/masteroraclesql/chapter1.1.c
@@ -1,9 +1,12 @@
 #include "stdio.h"
 #include "stdlib.h"
-void main(){
-int a;
-EXEC  SQL SELECT salary  INTO :a
-            FROM hr.employees
-            WHERE employee_id=108;
- printf("The salary is %d\n", a);
+#include <sqlca.h>
+
+int main(){
+    int a;
+    EXEC  SQL SELECT salary INTO :a
+        FROM hr.employees
+        WHERE employee_id=108;
+    printf("The salary is %d\n", a);
+    return 0;
 }
