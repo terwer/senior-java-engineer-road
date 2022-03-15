58a343b7dcfa086a98a24fa5239d2ab6009ea14b
Terwer <youweics@163.com>
2022-03-15 12:33:03 +0800

    feat:MyBatis入门案例

---
+++ p7-skill/framework/mybatis/mybatis-quickstarter/README.md
@@ -0,0 +1,2 @@
+# mybatis-quickstarter
+mybatis-quickstarter
---
+++ p7-skill/framework/mybatis/mybatis-quickstarter/src/main/java/com/terwergreen/test/MyBatisTest.java
@@ -0,0 +1,33 @@
+package com.terwergreen.test;
+
+import com.terwergreen.pojo.User;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.List;
+
+/**
+ * 测试类
+ *
+ * @name: MyBatisTest
+ * @author: terwer
+ * @date: 2022-03-15 12:17
+ **/
+public class MyBatisTest {
+    @Test
+    public void test1() throws IOException {
+        InputStream resourceAsStream = Resources.getResourceAsStream("sqlMapConfig.xml");
+        SqlSessionFactory sqlSessionFactory = new SqlSessionFactoryBuilder().build(resourceAsStream);
+
+        SqlSession sqlSession = sqlSessionFactory.openSession();
+        List<User> userList = sqlSession.selectList("user.findAll");
+        for (User user : userList) {
+            System.out.println(user);
+        }
+    }
+}
