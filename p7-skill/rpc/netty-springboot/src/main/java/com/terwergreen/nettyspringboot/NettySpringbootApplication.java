1c00f31458ef0b25491f5a6a3cf750a7c3912340
Terwer <youweics@163.com>
2022-04-30 23:07:55 +0800

    feat:初始化Netty的Websocket项目

---
+++ p7-skill/rpc/netty-springboot/src/main/java/com/terwergreen/nettyspringboot/NettySpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.terwergreen.nettyspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NettySpringbootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NettySpringbootApplication.class, args);
+    }
+
+}
---
+++ p7-skill/rpc/netty-springboot/src/test/java/com/terwergreen/nettyspringboot/NettySpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.terwergreen.nettyspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NettySpringbootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
