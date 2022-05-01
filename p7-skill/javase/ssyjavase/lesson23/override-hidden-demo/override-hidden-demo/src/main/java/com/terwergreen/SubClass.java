9b6beaf55a2b8349886fbe50b04c54b1c9325e12
Terwer <youweics@163.com>
2022-05-01 21:18:11 +0800

    feat:重载覆盖隐藏验证

--- p7-skill/javase/ssyjavase/lesson23/override-hidden-demo/override-hidden-demo/src/main/java/com/terwergreen/SubClass.java
+++ p7-skill/javase/ssyjavase/lesson23/override-hidden-demo/override-hidden-demo/src/main/java/com/terwergreen/SubClass.java
@@ -6,10 +6,10 @@ package com.terwergreen;
  * @date: 2022-05-01 21:04
  **/
 public class SubClass extends SuperClass {
-    //无论是不是static，都能隐藏父类的变量i
+    // 无论是不是static，都能隐藏父类的变量i
     public static int i = 2;
     public static int j = 1;
-    //无论是不是final，都能隐藏父类的变量k
+    // 无论是不是final，都能隐藏父类的变量k
     public final int k = 4;
 
     public static void method1() {
