125921e5c26d70a2173ed90eedf42643985ae34e
Terwer <youweics@163.com>
2022-10-12 23:32:14 +0800

    feat:Java SE之Object

--- p7-skill/javase/ssyjavase/javase-test/src/com/terwergreen/obj/ObjectTest.java
+++ p7-skill/javase/ssyjavase/javase-test/src/com/terwergreen/obj/ObjectTest.java
@@ -10,5 +10,28 @@ package com.terwergreen.obj;
 public class ObjectTest {
     public static void main(String[] args) {
         Object object = new Object();
+
+        System.out.println(object);
+        System.out.println(object.toString());
+
+        String str = "aaa";
+        System.out.println(str);
+        System.out.println(str.toString());
+
+        Student student = new Student();
+        System.out.println(student);
+        System.out.println(student.toString());
+
+        int v16 = 0xAB2F;
+        System.out.println(v16);
+
+        int result = 10 * (int) Math.pow(16, 3) + 11 * (int) Math.pow(16, 2) + 2 * 16 + 15;
+        System.out.println(result);
+    }
+}
+
+class Student {
+    public String toString() {
+        return "Hello World";
     }
 }
